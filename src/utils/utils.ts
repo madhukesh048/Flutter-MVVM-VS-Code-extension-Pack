f4f5bbf4e754d8b3068ed9bc692a5c20a7f16629
Ajil Oommen <ajilo297@gmail.com>
2019-12-14 11:46:24 +0530

    Moved files to utils

---
+++ src/utils/utils.ts
@@ -0,0 +1,45 @@
+import * as vscode from 'vscode';
+
+export class Utils {
+    public static isValidClassName(className: string): string | undefined {
+        if (className.length === 0) {
+            return "File name should have atleast 1 character";
+        }
+        if (className.toLowerCase() === "view") {
+            return "View is not a valid file name";
+        }
+        if (
+            !className
+                .substring(0, 1)
+                .match(new RegExp("([a-zA-Z$][w$]*.)*[a-zA-Z_$][w$]*"))
+        ) {
+            return "Invalid class name format";
+        }
+        return undefined;
+    }
+
+    public static openFile(filePath: string) {
+        console.info(`openFile: ${filePath}`);
+        let openPath = vscode.Uri.file(filePath);
+        vscode.workspace.openTextDocument(openPath).then((document) => {
+            vscode.window.showTextDocument(document);
+        });
+    }
+
+    public static processFileName(fileName: string): string {
+        console.log(`processFilename: fileName:${fileName}`);
+        if (fileName.length < 4) {
+            return fileName;
+        }
+        let lastFileName = fileName
+            .substring(fileName.length - 4, fileName.length)
+            .toLowerCase();
+    
+        if (lastFileName === "view") {
+            let truncatedFileName = fileName.substring(0, fileName.length - 4);
+            return truncatedFileName.trim();
+        }
+    
+        return fileName.trim();
+    }
+}
